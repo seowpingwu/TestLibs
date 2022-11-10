62678a3d11832736dc14091c2b76bcc69c96fc71
seowpingwu <seowpingwu@gmail.com>
2022-11-10 20:20:26 +0800

    更新1

---
+++ 00_TEST/test001/test001/test001.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+
+	int arr[10];
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	int max = arr[0];
+	for (i = 0; i<10; i++)
+	{
+		if (arr[i]>max)
+		{
+			max = arr[i];
+		}
+	}
+	printf("max value = %d\n", max);
+	return 0;
+}
+
+
